c1a2343c3e69751678c648cdfec407854e0260c3
Emil Dzhebrailov <emil.dzhebrailov@yahoo.com>
2022-03-07 10:54:13 -0500

    added showStuden

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,18 @@
 const functions = require("firebase-functions");
 
 exports.sayHello = functions.https.onRequest((req, res) => {
-    const message = "Hello my little friend.";
+    const message = "Hello my little friend";
     res.send(message);
+});
+
+exports.showStudent = functions.https.onRequest((req, res) => {
+    const student = {
+        firstName: 'Ruben',
+        lastName: 'Hales',
+        email: 'rubendog208@hotmail.com',
+        phone: '976-RUBE'
+    }
+    res.send(student);
 });
 
 // // Create and Deploy Your First Cloud Functions
